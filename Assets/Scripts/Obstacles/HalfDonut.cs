ba3320b0a88e1715856fc38498806e3d880614da
Beytullah Çetin <pasaport_07@hotmail.com>
2022-07-11 22:22:11 +0300

    create HalfDonut.cs

---
+++ Assets/Scripts/Obstacles/HalfDonut.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfDonut : MonoBehaviour
+{
+    [SerializeField] Transform donutStick;
+    [SerializeField] float speed;
+    [SerializeField] float distance = 0.1f;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            donutStick.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * speed, distance * 2) - distance, 0, 0);
+            ;
+
+            yield return null;
+        }
+    }
+}
