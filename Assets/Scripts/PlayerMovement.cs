8b65878272a5b02869c491eb7ed95362a218ddf9
Beytullah Çetin <pasaport_07@hotmail.com>
2022-07-12 19:35:02 +0300

    fix rotating platform

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,13 +12,18 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movementInput;
     Vector3 moveVector;
 
+    [SerializeField] Vector3 isGroundedOffset;
+
     [SerializeField] float movementSpeed = 1f;
     [SerializeField] float rotateSpeed = 1f;
-    const float gravity = -9.81f;
 
-    private void Update()
+    [SerializeField] float gravityForce;
+    Vector3 gravity;
+
+    private void LateUpdate()
     {
         Movement();
+        IsGrounded();
     }
 
     public void GetMovementInput(InputAction.CallbackContext context)
@@ -33,21 +38,42 @@ public class PlayerMovement : MonoBehaviour
     void Movement()
     {
         // Character Controller SimpleMove applies gravity to game object
-        characterController.SimpleMove(moveVector * movementSpeed);
         transform.forward = Vector3.Lerp(transform.forward, moveVector, rotateSpeed * Time.deltaTime);
 
-        if (characterController.isGrounded && moveVector.magnitude > 0)
+
+        if (IsGrounded())
         {
+            characterController.Move(moveVector * movementSpeed * Time.deltaTime);
             OnPlayerFall(false);
-            OnPlayerMovement(true);
+            OnPlayerMovement(moveVector.magnitude > 0 ? true : false);
         }
-        else if (moveVector.magnitude <= 0)
+        else
         {
-            OnPlayerMovement(false);
-        }
-        else if (false == characterController.isGrounded)
-        {
+            // This gravity is assigning seperately because of test purposes
+            // You can change gravity value from inspector
+            // and see the result
+            gravity = new Vector3(0, -gravityForce, 0);
+            characterController.Move((moveVector + gravity) * movementSpeed * Time.deltaTime);
             OnPlayerFall(true);
         }
+
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position + isGroundedOffset, -transform.up, Color.red, 1f);
+
+        if (Physics.Raycast(transform.position + isGroundedOffset, -transform.up, out hit, 1f))
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Platform"))
+            {
+                transform.SetParent(hit.transform);
+                return true;
+            }
+        }
+        transform.SetParent(null);
+        return false;
+
     }
 }
